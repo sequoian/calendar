6079bbaacf52e91593d81da3a49da1b36c41ce78
Zach Askins <zach303@msn.com>
2017-12-07 19:55:27 -0800

    user events route

--- server/routes/tests/events.test.js
+++ server/routes/tests/events.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const assert = require('chai').assert
+const expect = require('chai').expect
 const app = require('express')()
 const routes = require('..')
 const db = require('../../db')
@@ -12,16 +12,58 @@ app.use(bodyParser.json())
 app.use(cookieParser(process.env.COOKIE_KEY))
 app.use('/', routes)
 
+const addEvent = (title, owner) => {
+  return db.events.add({
+    title,
+    owner,
+    day: 'January 1, 2018',
+    time: '00:00',
+    location: null,
+    description: 'Testing'
+  })
+}
+
 describe.only ('event routes', () => {
+  beforeEach('clear tables', async () => {
+    await db.users.clearTable()
+    await db.events.clearTable()
+  })
 
-  describe ('GET events', () => {
-    it ('checks authentication')
+  describe.only ('GET events', () => {
+    const url = '/api/events'
 
-    it ('checks permission')
+    it ('checks authentication', () => {
+      return request(app)
+        .get(url)
+        .expect(401)
+    })
 
-    it ('gets all events belonging to user')
+    it ('gets all events belonging to user', async () => {
+      const user = await util.addUser(app)
+      await addEvent('Test', user.data.id)
 
-    it ('returns empty array when no events')
+      return request(app)
+        .get(url)
+        .set('Cookie', user.cookie)
+        .expect(200)
+        .then(res => {
+          const events = res.body.data.events
+          expect(events).to.have.length(1)
+        })
+    })
+
+    it ('returns empty array when no events', async () => {
+      const user = await util.addUser(app)
+
+      return request(app)
+        .get(url)
+        .set('Cookie', user.cookie)
+        .expect(200)
+        .then(res => {
+          const events = res.body.data.events
+          expect(events).to.have.length(0)
+        })
+    })
   })
 
   describe ('GET events/:id', () => {
