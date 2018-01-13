39c49746483f697a4ad848536f15c4b59f1d0e44
Zach Askins <zach303@msn.com>
2018-01-13 14:30:45 -0800

    password reset tables

--- server/users/sql/create.sql
+++ server/users/sql/create.sql
@@ -5,3 +5,9 @@ CREATE TABLE users (
   name          text,
   date_joined   timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE password_reset (
+  id              SERIAL PRIMARY KEY,
+  user_id         integer REFERENCES users ON DELETE CASCADE NOT NULL,
+  time_requested  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
