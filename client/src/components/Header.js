d4b41f87fa0db341d5627ab31517e89f585c8933
Zach Askins <zach303@msn.com>
2018-03-05 09:27:45 -0800

    deployed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import {changeDay} from '../actions'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import moment from 'moment'
 import '../css/Header.css'
 import NewEventBtn from './NewEventBtn'
 
